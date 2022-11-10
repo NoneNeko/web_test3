d27dd47173e2da5e63649bc5f70d05dd76587224
Dai Dung Lam <lamdaidung10c5@gmail.com>
2022-11-10 14:52:56 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /************************************************************************
-* WEB322–test 3
+* WEB322–test 4
 * I declare that this test is my own work in accordance with Seneca Academic
 Policy. No part * of this test has been copied manually or electronically from any
 other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Dai Dung Lam, Student ID: 137 632 196 Date: October-20th-2022
+* Name: Dai Dung Lam, Student ID: 137 632 196 Date: November-10-2022
 *
 * Your app’s URL (from Cyclic) : https://gold-repulsive-angelfish.cyclic.app/
 *
@@ -15,6 +15,7 @@ var express = require("express");
 var app = express();
 var dataPrep = require("./data_prep");
 app.use(express.static('public'));
+const exphbs = require("express-handlebars");
 
 app.use(express.json());
 
@@ -26,39 +27,51 @@ function onHttpStart(){
     console.log("Express http server listening on: "+ HTTP_PORT);
 }
 
+
+app.use(function(req,res,next){
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+   });
+
+app.engine(".hbs", exphbs.engine({
+    extname:".hbs",
+    defaultLayout: "main",
+    helpers: {
+        navLink: function(url, options){
+            return '<li' +
+            ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+            '><a href=" ' + url + ' ">' + options.fn(this) + '</a></li>';
+           }
+    }
+}));
+
+app.set("view engine", ".hbs");
+
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/home.html");
+   res.render("home");
 });
 
 app.get("/allStudents", (req,res) =>{
     dataPrep.allStudents().then((data) =>{
-        const student = data;
-        let resText = "<br>";
-        resText = JSON.stringify(student) + "<br>";
-        res.send(resText);
+        res.render("students", {students: data});
     }).catch((err) =>{
-        res.send("Message: ", err);
+        res.render({message: "no results"});
     })
 })
 
 app.get("/cpa", (req,res) =>{
     dataPrep.cpa().then((data) =>{
-        const cpaStud = data;
-        let resText = "<br>";
-        resText = JSON.stringify(cpaStud) + "<br>";
-        res.send(resText);
+        res.render("students", {students: data});
     }).catch((err) =>{
-        res.send("Message ", err);
+        res.render({message: "no results"});
     })
 })
 app.get("/highGPA", (req,res) =>{
     dataPrep.highGPA().then((data) =>{
-        const highStudent = data;
-        let resText = "<h1>Highest GPA: </h1>" + "Student ID: " + highStudent.studId + "<br>" + "Name: " + highStudent.name + "<br>"
-        + "Program: " + highStudent.program + "<br>" + "GPA: " + highStudent.gpa + "<br>";
-        res.send(resText);
+        res.render("student", {student: data});
     }).catch((err) =>{
-        res.send("Message: ", err);
+        res.render({message: "no results"});
     })
 })
 
@@ -70,32 +83,10 @@ app.get("/addStudent", (req,res) =>{
 app.post("/addStudent", (req, res)=>{
 
     dataPrep.addStudent(req.body).then(()=>{
-
         var data = req.body;
+        res.render("student", {student: data});
 
-        var txt =  ` <h2 style="color:red;"> The New Student Information  </h2>
-
-        <p> Student id: ${data.studId}</p>
-
-         <p> Student name: ${data.name} </p>
-
-        <p> Program: ${data.program} </p>
-
-        <p> GPA: ${data.gpa} </p>
-
-        <a href="/allStudents"> All Students </a> <br>
-
-        <a href="/"> Go Home </a>
-
-        `;
-
-        res.send(txt);
-
-        res.redirect("/allStudents");
-
-
-
-    }).catch((reason)=>res.json({message:reason}));
+    }).catch((reason)=>res.render({message: "no result"}));
 
 });
 
@@ -103,31 +94,9 @@ app.get("/student/:studId",(req, res)=>{
 
     dataPrep.getStudent(req.params.studId).then((data)=>{
 
-        var txt = `
+        res.render("student", {student: data});
 
-        <h2 style="color:red;"> This Student Information  </h2>
-
-        <p> Student id: ${data.studId}</p>
-
-        <p> Student name: ${data.name} </p>
-
-        <p> Program: ${data.program} </p>
-
-        <p> GPA: ${data.gpa} </p>
-
-        <a href="/allStudents"> Show All Students </a> <br>
-
-        <a href="/"> Go Home </a>
-
-        `;
-
-        res.send(txt);
-
-       // res.json(data);
-
-       // {"studId":3,"name":"name3","program":"BSD","gpa":3.3}
-
-    }).catch((reason)=>res.json({message:reason}));
+    }).catch((reason)=>res.render({message: "no result"}));
 
 });
 
